aeea163fe1e59c5b4ce0a0eca9c340357545fc16
yanina glaser <yglaser@mobilecomputing.com.ar>
2025-12-03 17:10:35 -0300

    fix test importings

--- src/app/shared/components/room-admin/room-create-card/room-create-card.spec.ts
+++ src/app/shared/components/room-admin/room-create-card/room-create-card.spec.ts
@@ -1,17 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RoomCreateCardComponent } from './room-create-card.component';
 
-import {  RoomAdminComponent } from './room-admin.component';
-
-describe('RoomAdmin', () => {
-  let component: RoomAdminComponent;
-  let fixture: ComponentFixture<RoomAdminComponent>;
+describe('RoomCreateCard', () => {
+  let component: RoomCreateCardComponent;
+  let fixture: ComponentFixture<RoomCreateCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RoomAdminComponent],
+      imports: [RoomCreateCardComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(RoomAdminComponent);
+    fixture = TestBed.createComponent(RoomCreateCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/shared/components/room-admin/room-list/room-list.spec.ts
+++ src/app/shared/components/room-admin/room-list/room-list.spec.ts
@@ -1,17 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RoomListComponent } from './room-list.component';
 
-import {  RoomAdminComponent } from './room-admin.component';
-
-describe('RoomAdmin', () => {
-  let component: RoomAdminComponent;
-  let fixture: ComponentFixture<RoomAdminComponent>;
+describe('RoomList', () => {
+  let component: RoomListComponent;
+  let fixture: ComponentFixture<RoomListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RoomAdminComponent],
+      imports: [RoomListComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(RoomAdminComponent);
+    fixture = TestBed.createComponent(RoomListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
