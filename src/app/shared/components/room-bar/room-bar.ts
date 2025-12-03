630cb75d9678731138640651c1f496808d1b564b
yanina glaser <yglaser@mobilecomputing.com.ar>
2025-12-03 10:38:15 -0300

    generate room component

---
+++ src/app/shared/components/room-bar/room-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-bar',
+  imports: [],
+  templateUrl: './room-bar.html',
+  styleUrl: './room-bar.css'
+})
+export class RoomBar {
+
+}
