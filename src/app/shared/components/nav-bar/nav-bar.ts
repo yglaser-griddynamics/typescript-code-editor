a3a3efa397bc170ea6ec5b2ca98e751a85402737
yanina glaser <yglaser@mobilecomputing.com.ar>
2025-12-05 16:39:02 -0300

    basic styling and make nabvar a separate component

---
+++ src/app/shared/components/nav-bar/nav-bar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.css',
+  standalone: false,
+})
+export class NavBar {}
--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -5,9 +5,9 @@ import { RoomAdminComponent } from './components/room-admin/room-admin.component
 
 import { RoomCreateCardComponent } from './components/room-admin/room-create-card/room-create-card.component';
 import { RoomListComponent } from './components/room-admin/room-list/room-list.component';
-
+import { NavBar } from './components/nav-bar/nav-bar';
 @NgModule({
-  declarations: [RoomAdminComponent, RoomCreateCardComponent, RoomListComponent],
+  declarations: [NavBar,RoomAdminComponent, RoomCreateCardComponent, RoomListComponent],
   imports: [CommonModule, FormsModule],
   exports: [
     RoomAdminComponent,
@@ -15,6 +15,7 @@ import { RoomListComponent } from './components/room-admin/room-list/room-list.c
     CommonModule,
     RoomCreateCardComponent,
     RoomListComponent,
+    NavBar
   ],
 })
 export class SharedModule {}
